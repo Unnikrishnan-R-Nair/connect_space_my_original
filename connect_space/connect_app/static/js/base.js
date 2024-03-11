55df8a29cae28e4efccb8a8d899cf6cc7d0fb85c
Unnikrishnan R Nair <rajendranunnikrishnan7@gmail.com>
2024-03-11 20:05:45 +0530

    Navbar hover effect

--- connect_space/connect_app/static/js/base.js
+++ connect_space/connect_app/static/js/base.js
@@ -10,3 +10,34 @@ window.addEventListener('resize', function(e){
         document.querySelector('.mynavbar .mobile-navbar').classList.add('hidden');
     }
 })
+
+window.addEventListener('scroll', function(e){
+    if (this.scrollY > 100 && !document.querySelector('.mynavbar').classList.contains('navhover')) {
+        document.querySelector('.mynavbar').classList.add('after-scroll');
+        document.querySelector('.mynavbar').style.paddingBottom = '15px';
+    }else if (this.scrollY < 100){
+        document.querySelector('.mynavbar').classList.remove('after-scroll');      
+        document.querySelector('.mynavbar').style.paddingBottom = 'initial';
+
+    } 
+})
+
+
+document.querySelector('.mynavbar').addEventListener('mouseenter', function(e){
+    console.log('entered');
+    this.classList.add('navhover')
+    this.style.paddingBottom = 'initial';
+
+
+})
+document.querySelector('.mynavbar').addEventListener('mouseleave', function(e){
+    console.log('left');
+    this.classList.remove('navhover')
+    if (window.scrollY < 100){
+        this.style.paddingBottom = 'initial';
+    } else if (window.scrollY > 100) {
+
+        this.style.paddingBottom = '15px';
+    }
+
+})
