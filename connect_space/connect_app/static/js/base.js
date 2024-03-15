81f23c8535dba29c4d996b5f25488ea1766f8fd1
Unnikrishnan R Nair <rajendranunnikrishnan7@gmail.com>
2024-03-15 09:27:41 +0530

    Navbar event listeners corrected

--- connect_space/connect_app/static/js/base.js
+++ connect_space/connect_app/static/js/base.js
@@ -1,17 +1,19 @@
 document.querySelector('.mynavbar .hamburger-btn').addEventListener('click', function(e){
     //console.log('clicked')
-
+    e.stopPropagation()
     document.querySelector('.mynavbar .mobile-navbar').classList.toggle('hidden');
 
 })
 
 window.addEventListener('resize', function(e){
+    e.stopPropagation();
     if(this.innerWidth >= 1024) {
         document.querySelector('.mynavbar .mobile-navbar').classList.add('hidden');
     }
 })
 
 window.addEventListener('scroll', function(e){
+    e.stopPropagation();
     if (this.scrollY > 100 && !document.querySelector('.mynavbar').classList.contains('navhover')) {
         document.querySelector('.mynavbar').classList.add('after-scroll');
         document.querySelector('.mynavbar').style.paddingBottom = '15px';
@@ -24,6 +26,7 @@ window.addEventListener('scroll', function(e){
 
 
 document.querySelector('.mynavbar').addEventListener('mouseenter', function(e){
+    e.stopPropagation()
     // console.log('entered');
     this.classList.add('navhover')
     this.style.paddingBottom = 'initial';
@@ -31,6 +34,7 @@ document.querySelector('.mynavbar').addEventListener('mouseenter', function(e){
 
 })
 document.querySelector('.mynavbar').addEventListener('mouseleave', function(e){
+    e.stopPropagation()
     // console.log('left');
     this.classList.remove('navhover')
     if (window.scrollY < 100){
